4a44ecc808bbdb82f585514333a536b95fae7a13
SHUBHAM VERMA <60481686+shubham9471@users.noreply.github.com>
2022-01-24 19:22:19 +0530

    updated code

---
+++ src/Middleware/userAuth.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken')
+
+
+const userAuth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization' , 'Bearer Token')
+
+
+        if(!token){
+            return res.status(403).send({ status: false, message: `Missing authentication token in request` })
+        }
+       
+        let x=token.split(' ')
+ 
+        let timeOut = jwt.decode(x[1],'project 5-cart') 
+        
+       
+       
+        //let decoded = jwt.verify(token, 'project4')
+        
+        if (!timeOut) {
+            return res.status(403).send({ status: false, message: `Invalid authentication token in request ` })
+        }
+
+        if (Date.now() > (timeOut.exp)*1000 ){
+            return res.status(404).send({ status: false, message: `Session Expired, please login again` })
+        }
+
+
+
+        req.userId = timeOut.userId
+
+        next()
+    } catch (error) {
+        console.error(`Error! ${error.message}`)
+        res.status(500).send({ status: false, message: error.message })
+    }
+}
+
+
+module.exports = {
+    userAuth
+}
